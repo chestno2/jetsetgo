ef564f497b0879a28f034f7dccc58a9c6cb3eaa3
chestno2 <atuly152001@gmail.com>
2024-03-13 20:54:35 +0530

    lottie commit

--- src/Components/Flightgif.js
+++ src/Components/Flightgif.js
@@ -3,11 +3,15 @@ import React from 'react'
 import flightgifi from "../Assets/flightanimation.json"
 import Lottie from "lottie-react";
 
-
+const style ={
+  width:"100vw",
+  height:"100vh",
+  overflow:"hidden"
+}
 
 function Flightgif() {
   return (
-    <Lottie animationData={flightgifi} loop={true} autoplay={true} />
+    <Lottie style={style} animationData={flightgifi} loop={true} autoplay={true} />
   )
 }
 
